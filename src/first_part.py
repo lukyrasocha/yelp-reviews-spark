74ebccd379f041b56a28e4b1b3fef0d56072a67e
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-04-07 23:54:05 +0200

    move to src

---
+++ src/first_part.py
@@ -0,0 +1,55 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql.functions import countDistinct, col
+
+conf = SparkConf()
+conf.set("spark.executor.memory", "2G")
+conf.set("spark.executor.instances", "4")
+
+spark = SparkSession.builder \
+                    .appName('template-application') \
+                    .config(conf=conf) \
+                    .getOrCreate()
+
+bs = spark.read.json("/datasets/yelp/business.json")
+rs = spark.read.json ("/datasets/yelp/review.json")
+us = spark.read.json("/datasets/yelp/user.json")
+
+# SPECIFIC DATAFRAME QUERIES
+## Question 1 - Total number of reviews
+print('Q1 - Total number of reviews')
+bs.agg({'review_count': 'sum'}).show()
+print('Q1 DONE')
+
+## Question 2 - filter some businesses
+print('Q2 - businesses with 5 stars that have been reviewed by at least 1000 people')
+filtered = bs.filter(bs.stars == 5).filter(bs.review_count >= 1000).select("name","stars","review_count")
+filtered.show()
+print('Q2 DONE')
+
+## Question 3 - filter users that wrote more than 1000 reviews
+print('Q3 - influencers (people who wrote more than 1000 reviews)')
+influencers = us.filter(us.review_count > 1000).select("user_id")
+influencers.show()
+print('Q3 DONE')
+
+## Question 4 - find businesses that have been reviewed by more than 5 influencers
+print('Q4 - businesses that have been reviewed by more than 5 distinct influencers')
+#Filter only the reviews made by the influencers
+reviews_by_influencers = rs.join(influencers, ["user_id"], 'leftsemi')
+#Find the distinct counts of influencer users per business_id
+gr = reviews_by_influencers.groupBy('business_id').agg(countDistinct('user_id').alias('influencer_count'))
+#Take only the businesses that have more than 5 reviews from influencers
+grFiltered = gr.filter(gr.influencer_count > 5)
+#Find the corresponding business names
+business_names = bs.join(grFiltered, bs.business_id == grFiltered.business_id, "inner").select("name")
+business_names.show()
+print('Q4 DONE')
+
+# Question 5 - Find an ordered list of users based on the average star count given in all their reviews
+print("Q5 - ordered list of users based on the average star count given in all their reviews")
+avgStars = rs.groupBy('user_id').avg('stars')
+user_names = us.join(avgStars, us.user_id == avgStars.user_id, "inner").orderBy('avg(stars)',ascending=False).select("name") #, "avg(st    ars)", "average_stars")
+user_names.show()
+print("Q5 DONE")
---
+++ src/second_part.py
@@ -0,0 +1,68 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql.functions import countDistinct, col, explode, split
+
+conf = SparkConf()
+conf.set("spark.executor.memory", "2G")
+conf.set("spark.executor.instances", "4")
+
+spark = SparkSession.builder \
+                    .appName('template-application') \
+                    .config(conf=conf) \
+                    .getOrCreate()
+
+bs = spark.read.json("/datasets/yelp/business.json")
+rs = spark.read.json ("/datasets/yelp/review.json")
+
+# What is the percentage of reviews containing a variant of the word 'authentic'?
+print('-----What is the percentage of reviews containing a variant of the word "authentic"------')
+restaurants = bs[bs.categories.contains('Restaurants')]
+res_bus = restaurants.join(rs, "business_id")
+
+# Find reviews that have a variant of the word "authentic"
+res_bus_auth = res_bus.filter(res_bus.text.rlike('[Aa]uthentic[a-z]*'))
+res_bus_auth_percentage = (res_bus_auth.count()/res_bus.count())*100
+
+print(f"Percentage of authentic reviews out of all restaurants reviews {res_bus_auth_percentage} %")
+
+#How many reviews contain the string "legitimate" grouped by businesses type (type of cuisine)?
+
+# Explode columns - since 'categories' is a long string of multiple categories for each row
+# we first split all the categories to get a list of categories for each row
+# then we explode it, what it does is that it basically creates a row for each of the items in the list
+# For example if we have a row('James', ['Python', 'Java']) we will get
+# row('James', 'Python'), row('James', 'Java')
+
+print('======= How many reviews contain "legitimate" grouped by restaurant categories=======')
+res_bus_exploded = res_bus.withColumn('single_categories', explode(split(col('categories'), ', ')))
+#Group them by the single categories and count how many of each single category are there
+res_bus_grouped = res_bus_exploded.groupBy("single_categories").count()
+res_bus_grouped = res_bus_grouped.withColumnRenamed("count", "all_count")
+
+# Now lets do the same but only with the ones containing the word 'legitimate'
+
+# Reviews containing the word 'legitimate'
+res_bus_legitimate = res_bus.filter(res_bus.text.contains("legitimate"))
+
+#rs_bus_legitimate = rs_legitimate.join(bs, "business_id")
+res_bus_legit_exploded = res_bus_legitimate.withColumn('single_categories', explode(split(col('categories'), ', ')))
+res_bus_legit_grouped = res_bus_legit_exploded.groupBy("single_categories").count()
+res_bus_legit_grouped = res_bus_legit_grouped.withColumnRenamed("count", "legit_count")
+
+# Normalize by the count - so you know what percentage of the category contains the word legitimate
+legit_ratio = res_bus_legit_grouped.join(res_bus_grouped, "single_categories")
+legit_ratio = legit_ratio.withColumn('ratio', legit_ratio.legit_count/legit_ratio.all_count)
+legit_ratio.sort('all_count', ascending=False).show()
+
+# How many reviews contain the string "legitimate" groupedby restaurant categories?
+cuisines = ['Chinese', 'Japanese', 'Korean', 'Mexican', 'Eastern European', 'American', 'French', 'Italian', 'Greek', 'Indian']
+legit_ratio.filter(legit_ratio.single_categories.isin(cuisines)).sort('ratio', ascending=False).show()
+
+# Is there a difference in the amount of authenticity language used in the different areas?
+print('========== Difference in the amount of authenticty language used in different areas ========')
+res_bus = res_bus.withColumn("auth_lang", res_bus.text.rlike('([Ll]egitimate)|([Aa]uthentic[a-z]*)'))
+
+#The cube function “takes a list of columns and applies aggregate expressions to all possible combinations of the grouping columns”
+res_bus_cube = res_bus.cube("state", "city", "auth_lang").count().orderBy("count", ascending=False)
+res_bus_cube.show()
