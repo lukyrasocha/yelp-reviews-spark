c135ac2aa8264f1ef4300407b90663ba14e86471
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-05-13 18:33:43 +0200

    Update

---
+++ src/correlation_test.py
@@ -0,0 +1,44 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql.functions import countDistinct, col, explode, split
+from functools import reduce
+
+conf = SparkConf()
+conf.set("spark.executor.memory", "2G")
+conf.set("spark.executor.instances", "4")
+
+spark = SparkSession.builder \
+                    .appName('template-application') \
+                    .config(conf=conf) \
+                    .getOrCreate()
+
+bs = spark.read.json("/datasets/yelp/business.json")
+rs = spark.read.json ("/datasets/yelp/review.json")
+restaurants = bs[bs.categories.contains('Restaurants')]
+restaurants = restaurants.withColumnRenamed('stars', 'business_stars')
+
+non_europe = ['South American', 'African','Mexican', 'Argentine', 'Bolivian', 'Chilean', 'Ecuadorian', 'Peruvian', \
+              'Uruguayan', 'Colombian', 'Paraguayan', 'Venezielean', 'Asian', 'Japanese', 'Chinese', 'Mongolian' , \
+              'Turkish', 'Korean','Indian','Pakistani','Bangladeshi','Thai','Singaporean', 'Brazilian', \
+              'Vietnamese', 'Indonesian', 'Cambodian', 'Burmese', 'Malaysian', 'Filipino', \
+              'Arab', 'Iranian','Taiwanese', 'Russian', 'Latin American']
+
+safe_cuisines = ['European', 'French', 'Italian', 'German', 'Danish', 'Greek', '^American', 'Austrian']
+
+# Join reviews and business information together
+res_bus = restaurants.join(rs, "business_id")
+
+# Explode columns to get single categories
+res_bus_explode = res_bus.withColumn('single_categories', explode(split(col('categories'),', ')))
+
+non_europe = res_bus_explode.filter(res_bus_explode.single_categories.rlike("|".join(non_europe)))
+europe_like = res_bus_explode.filter(res_bus_explode.single_categories.rlike("|".join(safe_cuisines)))
+
+non_europe_corr = non_europe.withColumn('AuthComment', (non_europe.text.rlike('([Ll]egitimate)|([Aa]uthentic[a-z]*)').cast('integer')))
+europe_corr = europe_like.withColumn('AuthComment', (europe_like.text.rlike('([Ll]egitimate)|([Aa]uthentic[a-z]*)').cast('integer')))
+
+print('CORRELATION FOR NON_EUROPE')
+print(non_europe_corr.stat.corr('AuthComment', 'stars'))
+print('CORRELATION FOR EUROPE')
+print(europe_corr.stat.corr('AuthComment', 'stars'))
--- src/second_part.py
+++ src/second_part.py
@@ -21,10 +21,10 @@ restaurants = bs[bs.categories.contains('Restaurants')]
 res_bus = restaurants.join(rs, "business_id")
 
 # Find reviews that have a variant of the word "authentic"
-res_bus_auth = res_bus.filter(res_bus.text.rlike('[Aa]uthentic[a-z]*'))
-res_bus_auth_percentage = (res_bus_auth.count()/res_bus.count())*100
+#res_bus_auth = res_bus.filter(res_bus.text.rlike('[Aa]uthentic[a-z]*'))
+#res_bus_auth_percentage = (res_bus_auth.count()/res_bus.count())*100
 
-print(f"Percentage of authentic reviews out of all restaurants reviews {res_bus_auth_percentage} %")
+#print(f"Percentage of authentic reviews out of all restaurants reviews {res_bus_auth_percentage} %")
 
 #How many reviews contain the string "legitimate" grouped by businesses type (type of cuisine)?
 
@@ -35,30 +35,30 @@ print(f"Percentage of authentic reviews out of all restaurants reviews {res_bus_
 # row('James', 'Python'), row('James', 'Java')
 
 print('======= How many reviews contain "legitimate" grouped by restaurant categories=======')
-res_bus_exploded = res_bus.withColumn('single_categories', explode(split(col('categories'), ', ')))
+#res_bus_exploded = res_bus.withColumn('single_categories', explode(split(col('categories'), ', ')))
 #Group them by the single categories and count how many of each single category are there
-res_bus_grouped = res_bus_exploded.groupBy("single_categories").count()
-res_bus_grouped = res_bus_grouped.withColumnRenamed("count", "all_count")
+#res_bus_grouped = res_bus_exploded.groupBy("single_categories").count()
+#res_bus_grouped = res_bus_grouped.withColumnRenamed("count", "all_count")
 
 
 # Now lets do the same but only with the ones containing the word 'legitimate'
 
 # Reviews containing the word 'legitimate'
-res_bus_legitimate = res_bus.filter(res_bus.text.contains("legitimate"))
+#res_bus_legitimate = res_bus.filter(res_bus.text.contains("legitimate"))
 
 #rs_bus_legitimate = rs_legitimate.join(bs, "business_id")
-res_bus_legit_exploded = res_bus_legitimate.withColumn('single_categories', explode(split(col('categories'), ', ')))
-res_bus_legit_grouped = res_bus_legit_exploded.groupBy("single_categories").count()
-res_bus_legit_grouped = res_bus_legit_grouped.withColumnRenamed("count", "legit_count")
+#res_bus_legit_exploded = res_bus_legitimate.withColumn('single_categories', explode(split(col('categories'), ', ')))
+#res_bus_legit_grouped = res_bus_legit_exploded.groupBy("single_categories").count()
+#res_bus_legit_grouped = res_bus_legit_grouped.withColumnRenamed("count", "legit_count")
 
 # Normalize by the count - so you know what percentage of the category contains the word legitimate
-legit_ratio = res_bus_legit_grouped.join(res_bus_grouped, "single_categories")
-legit_ratio = legit_ratio.withColumn('ratio', legit_ratio.legit_count/legit_ratio.all_count)
-legit_ratio.sort('all_count', ascending=False).show()
+#legit_ratio = res_bus_legit_grouped.join(res_bus_grouped, "single_categories")
+#legit_ratio = legit_ratio.withColumn('ratio', legit_ratio.legit_count/legit_ratio.all_count)
+#legit_ratio.sort('all_count', ascending=False).show()
 
 # How many reviews contain the string "legitimate" groupedby restaurant categories?
-cuisines = ['Chinese', 'Japanese', 'Korean', 'Mexican', 'Eastern European', 'American', 'French', 'Italian', 'Greek', 'Indian']
-legit_ratio.filter(legit_ratio.single_categories.isin(cuisines)).sort('ratio', ascending=False).show()
+#cuisines = ['Chinese', 'Japanese', 'Korean', 'Mexican', 'Eastern European', 'American', 'French', 'Italian', 'Greek', 'Indian']
+#legit_ratio.filter(legit_ratio.single_categories.isin(cuisines)).sort('ratio', ascending=False).show()
 
 # Is there a difference in the amount of authenticity language used in the different areas?
 print('========== Difference in the amount of authenticty language used in different areas ========')
@@ -66,6 +66,9 @@ res_bus = res_bus.withColumn("auth_lang", res_bus.text.rlike('([Ll]egitimate)|([
 
 #The cube function “takes a list of columns and applies aggregate expressions to all possible combinations of the grouping columns”
 res_bus_cube = res_bus.cube("state", "city", "auth_lang").count().orderBy("count", ascending=False)
-res_bus_cube.show()
+res_bus_cube_states = res_bus_cube.filter(res_bus_cube.auth_lang.isNull()).filter(res_bus_cube.city.isNull()).withColumnRenamed("count", "reviews_count").drop("city","auth_lang")
+res_bus_cube_true = res_bus_cube.filter(res_bus_cube["auth_lang"] == True).filter(res_bus_cube.city.isNull()).withColumnRenamed("count", "auth_count").withColumnRenamed("state","state2").drop("city","auth_lang")
 
-
+together = res_bus_cube_states.join(res_bus_cube_true,res_bus_cube_states.state ==  res_bus_cube_true.state2,"inner").drop('state2')
+together = together.withColumn('ratio', (together.auth_count/together.reviews_count)*100).sort('ratio',ascending=False)
+together.show()
